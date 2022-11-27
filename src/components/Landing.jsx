48d6a813c0fdc6db982eba7404944904c8324860
Adem <ademet123676@icloud.com>
2022-11-27 21:59:51 +1100

    update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
     <header>
         <div className="header__container">
         <div className="header__description">
-            <h1>America's most awarded online library platform</h1>
+            <h1>Australia's most awarded online library platform</h1>
             <h2>Find your dream book with <span className="purple">Library</span></h2>
             <a href="#features">
               <button className="btn">Browse books</button>
